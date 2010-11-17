3d05a88d6000bbee03267dbd102fc55c5571e272
Brandon M. West <brawest@gmail.com>
2010-11-17 16:33:03 +0200

    canvas creation now working on test.html

--- dominvaders.js
+++ dominvaders.js
@@ -1,4 +1,4 @@
-domInvaders.prototype = {
+var domInvaders = {
 	initialize: function() {
 		this.getConfiguration();
 		this.setupCanvas();
@@ -8,23 +8,34 @@ domInvaders.prototype = {
 	getConfiguration: function() {
 		this.playerWidth = 50;
 		this.playerHeight = 20;
+		// units/second
+		this.acc = 300;
+		this.maxSpeed = 600;
 		
-		this.w = document.documentElement.clientWidth;
-		this.h = document.documentElement.clientHeight;
+		this.bulletSpeed = 700;
+		this.particleSpeed = 400;
+		this.timeBetweenFire = 150; // how many milliseconds between shots
+		this.timeBetweenEnemyFire = 150;
+		this.bulletRadius = 2;
+		this.maxParticles = 40;
+		this.maxBullets = 10;
 	},
 	
 	setupCanvas: function(){
+		w = document.documentElement.clientWidth;
+		h = document.documentElement.clientHeight;
+		
 		this.container = document.createElement('div');
 		this.container.className = 'domInvadersContainer';
 		document.body.appendChild(this.container);
 
 		this.canvas = document.createElement('canvas');
-		this.canvas.setAttribute('width', this.w);
-		this.canvas.setAttribute('height', this.h);
+		this.canvas.setAttribute('width', w);
+		this.canvas.setAttribute('height', h);
 		this.canvas.className = 'domInvadersCanvas';
 		with ( this.canvas.style ) {
-			width = this.w + "px";
-			height = this.h + "px";
+			width = w + "px";
+			height = h + "px";
 			position = "fixed";
 			top = "0px";
 			left = "0px";
---
+++ utilities.js
@@ -0,0 +1,52 @@
+// Taken from:
+// http://www.quirksmode.org/blog/archives/2005/10/_and_the_winner_1.html
+function addEvent( obj, type, fn ) {
+	if (obj.addEventListener)
+		obj.addEventListener( type, fn, false );
+	else if (obj.attachEvent) {
+		obj["e"+type+fn] = fn;
+		obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
+		obj.attachEvent( "on"+type, obj[type+fn] );
+	}
+}
+
+function removeEvent( obj, type, fn ) {
+	if (obj.removeEventListener)
+		obj.removeEventListener( type, fn, false );
+	else if (obj.detachEvent) {
+		obj.detachEvent( "on"+type, obj[type+fn] );
+		obj[type+fn] = null;
+		obj["e"+type+fn] = null;
+	}
+}
+
+// taken from MooTools Core
+function addClass(element, className) {
+	if ( element.className.indexOf(className) == -1)
+		element.className = (element.className + ' ' + className).replace(/\s+/g, ' ').replace(/^\s+|\s+$/g, '');
+};
+
+// taken from MooTools Core
+function removeClass(element, className) {
+	element.className = element.className.replace(new RegExp('(^|\\s)' + className + '(?:\\s|$)'), '$1');
+};
+
+function addStylesheet(selector, rules) {
+	var stylesheet = document.createElement('style');
+	stylesheet.type = 'text/css';
+	stylesheet.rel = 'stylesheet';
+	stylesheet.id = 'ASTEROIDSYEAHSTYLES';
+	try {
+		stylesheet.innerHTML = selector + "{" + rules + "}";
+	} catch ( e ) {
+		stylesheet.styleSheet.addRule(selector, rules);
+	}
+	document.getElementsByTagName("head")[0].appendChild(stylesheet);
+};
+
+function removeStylesheet(name) {
+	var stylesheet = document.getElementById(name);
+	if ( stylesheet ) {
+		stylesheet.parentNode.removeChild(stylesheet);
+	}
+};
